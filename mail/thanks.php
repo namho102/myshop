7dc31d1948354655c449ec0c50179194e453465a
namho102 <namhoang.td09@gmail.com>
2014-10-15 19:04:43 +0700

    thanks.php

---
+++ mail/thanks.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+    <meta charset="utf-8">
+	<title>Thanks for submitting</title>
+
+	<link href="../css/m_styles.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<div class="container">
+        <br /><br />
+        <h1>Your message has been sent!</h1><br />
+		
+		<p><a href="index.html">Back to Home</a></p>
+	
+	</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
